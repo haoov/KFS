382e1c8febdf53aa36970ae0d592aaa843b83bd6
Juventz ARISTIL MATHURIN <jaristil@paul-f4Ar7s3.clusters.42paris.fr>
2025-03-18 17:57:09 +0100

    bootloader in ASM, x32, compile, 2st version

--- src/boot.asm
+++ src/boot.asm
@@ -4,23 +4,41 @@ section .multiboot ;                multiboot header that marks the program as a
     FLAGS equ 0x0 ;                 0 means no memory Or modules are required.
     CHECKSUM equ -(MAGIC + FLAGS) ; Ensure the sum of MAGIC + FLAGS + CHECKSUM is 0.
 
-section .data ;                     Declare multiboot variables
-    resd 1 ; MAGIC
-    resd 1 ; FLAGS
-    resd 1 ; CHECKSUM
+    dd MAGIC
+    dd FLAGS
+    dd CHECKSUM
 
 section .bss ;                      Contains uninitialised variables (Block Started by Symbol)
-    resb 8192 ;                     Reserv 8 KB for the stack (8192 octets)
+    stack_bottom:
+        resb  1048576 ;             Reserv 8 KB for the stack (8192 octets)
     stack_top: ;                    Label for stack adress
 
 section .text ;                     Define a code section where the program is stored. Program's intruction go there.
+    bits 32
     global _start ;                 Specify _start as the entry point to the kernel and the bootloader will jump to this position once loaded.
+    extern k_main                   Declare that a k_main exist in an other file.
 
 _start:
-    mov esp, stack_top + 8192 ;     Set the esp register to point the top of the stack (it grows downwards on x86 system).
+    mov esp, stack_top ;             Set the esp register to point the top of the stack (it grows downwards on x86 system).
     
+    call check_multiboot ;          Check that multiboot is conform.
+
     call k_main ;                   Call to the kernel main function defined in the C in our kernel.
 
-    cli ;                           Disable interrupts on the processor. To do before manipulating hardware or starting a task in the kernel.
-    
     hlt ;                           Stops the execution of the processor while waiting for new events or instructions.
+
+check_multiboot:
+    cmp eax, 0x36d76289             Compare the value in eax and magic number
+    jne .no_multiboot               If it do not correspond, then jump to no_multiboot
+    ret
+
+.no_multiboot:
+    move al, "0"
+    jmp error
+
+error:
+    mov dword [0xb8000], 0x4f524f45  ; "OORE" (O) et "R" pour "ERROR"
+    mov dword [0xb8004], 0x4f3a4f52  ; "OR: " et "ER" pour "ROR"
+    mov dword [0xb8008], 0x4f204f20  ; "O " (O pour error)
+    mov byte  [0xb800a], al
+    hlt
