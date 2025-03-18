2d24d6c748fce9a99427a5c3ed613ccda2fd22a7
Juventz ARISTIL MATHURIN <jaristil@paul-f4Ar7s3.clusters.42paris.fr>
2025-03-18 17:25:17 +0100

    bootloader in ASM, x32, compile, 1st version

--- src/boot.asm
+++ src/boot.asm
@@ -1,26 +1,26 @@
-section .multiboot ;            multiboot header that marks the program as a kernel and define the data required for multiboot startup.
-align 8 ;                       to align the data in a border of 4 octets (each data will be stored at an adress who is a x4).
+section .multiboot ;                multiboot header that marks the program as a kernel and define the data required for multiboot startup.
+    align 4 ;                       to align the data in a border of 4 octets (each data will be stored at an adress who is a x4).
+    MAGIC equ 0x1BADB002 ;          Magic number as defined in the multiboot specification.
+    FLAGS equ 0x0 ;                 0 means no memory Or modules are required.
+    CHECKSUM equ -(MAGIC + FLAGS) ; Ensure the sum of MAGIC + FLAGS + CHECKSUM is 0.
 
-MAGIC equ 0x1BADB002 ;          Magic number as defined in the multiboot specification.
-FLAGS equ 0x0 ;                 0 means no memory Or modules are required.
-CHECKSUM equ -(MAGIC + FLAGS) ; Ensure the sum of MAGIC + FLAGS + CHECKSUM is 0.
+section .data ;                     Declare multiboot variables
+    resd 1 ; MAGIC
+    resd 1 ; FLAGS
+    resd 1 ; CHECKSUM
 
-long MAGIC
-long FLAGS
-long CHECKSUM
+section .bss ;                      Contains uninitialised variables (Block Started by Symbol)
+    resb 8192 ;                     Reserv 8 KB for the stack (8192 octets)
+    stack_top: ;                    Label for stack adress
 
-section .bss ;                  Contains uninitialised variables (Block Started by Symbol)
-resb 8192 ;                     Reserv 8 KB for the stack (8192 octets)
-stack_top: ;                    Label for stack adress
-
-section .text ;                 Define a code section where the program is stored. Program's intruction go there.
-global _start ;                 Specify _start as the entry point to the kernel and the bootloader will jump to this position once loaded.
+section .text ;                     Define a code section where the program is stored. Program's intruction go there.
+    global _start ;                 Specify _start as the entry point to the kernel and the bootloader will jump to this position once loaded.
 
 _start:
-    mov esp, stack_top ;        Set the esp register to point the top of the stack (it grows downwards on x86 system).
+    mov esp, stack_top + 8192 ;     Set the esp register to point the top of the stack (it grows downwards on x86 system).
     
-    call k_main ;               Call to the kernel main function defined in the C in our kernel.
+    call k_main ;                   Call to the kernel main function defined in the C in our kernel.
 
-    cli ;                       Disable interrupts on the processor. To do before manipulating hardware or starting a task in the kernel.
+    cli ;                           Disable interrupts on the processor. To do before manipulating hardware or starting a task in the kernel.
     
-    hlt ;                       Stops the execution of the processor while waiting for new events or instructions.
+    hlt ;                           Stops the execution of the processor while waiting for new events or instructions.
