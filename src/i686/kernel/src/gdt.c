4fb2340bec42544a3ab0f51dbb75a8f9458b0708
Raphael SABBAH <rsabbah@bess-f2r2s12.clusters.42paris.fr>
2025-03-26 19:47:24 +0100

    kernel and user data code and stack segment set and gdt address set

--- src/i686/kernel/src/gdt.c
+++ src/i686/kernel/src/gdt.c
@@ -1,9 +1,15 @@
 #include "gdt.h"
+#include "intf.h"
 
-struct gdt_entry	gdt[3];
+__attribute__((section(".gdt")))
+__attribute__((aligned(4)))
+struct gdt_entry	gdt[7];
+
+__attribute__((section(".gdt")))
+__attribute__((aligned(4)))
 struct gdt_ptr		gdtp;
 
-void	gdt_set_entry(int n, uint32_t b, uint32_t l, uint8_t a, uint8_t g)
+void	gdt_set_gate(int n, uint32_t b, uint32_t l, uint8_t a, uint8_t g)
 {
 	gdt[n].base_lo = (b & 0xFFFF);
 	gdt[n].base_mid = (b >> 16) & 0xFF;
@@ -18,14 +24,42 @@ void	gdt_set_entry(int n, uint32_t b, uint32_t l, uint8_t a, uint8_t g)
 
 void	gdt_install(void)
 {
-	gdtp.limit = (sizeof (struct gdt_entry) * 3) - 1;
+	gdtp.limit = (sizeof (struct gdt_entry) * 7) - 1;
 	gdtp.base = (uint32_t)&gdt;
 
-	gdt_set_entry(0, 0, 0, 0, 0);
+	kprint_hex(gdtp.base);
 
-	gdt_set_entry(1, 0, 0xFFFFFFFF, 0x9A, 0xCF);
+	gdt_set_gate(0, 0, 0, 0, 0);
 
-	gdt_set_entry(2, 0, 0xFFFFFFFF, 0x93, 0xCF);
+	// Kernel code segment
+	gdt_set_gate(	1, 0, MEM_LIMIT,
+					ACC_PRESENT | ACC_R0 | ACC_DESC | ACC_EXEC | ACC_CODE_RD,
+					FLAG_GRAN4K | FLAG_32BIT);
 
-	(void)gdt_flush();
+	// Kernel data segment
+	gdt_set_gate(	2, 0, MEM_LIMIT,
+					ACC_PRESENT | ACC_R0 | ACC_DESC | ACC_DATA_WR,
+					FLAG_GRAN4K | FLAG_32BIT);
+
+	// Kernel stack segment
+	gdt_set_gate(	3, 0, MEM_LIMIT,
+					ACC_PRESENT | ACC_R0 | ACC_DESC | ACC_DATA_WR,
+					FLAG_GRAN4K | FLAG_32BIT);
+
+	// User code segment
+	gdt_set_gate(	4, 0, MEM_LIMIT,
+					ACC_PRESENT | ACC_R3 | ACC_DESC | ACC_EXEC | ACC_CODE_RD,
+					FLAG_GRAN4K | FLAG_32BIT);
+
+	// User data segment
+	gdt_set_gate(	5, 0, MEM_LIMIT,
+					ACC_PRESENT | ACC_R3 | ACC_DESC | ACC_DATA_WR,
+					FLAG_GRAN4K | FLAG_32BIT);
+
+	// User stack segment
+	gdt_set_gate(	6, 0, MEM_LIMIT,
+					ACC_PRESENT | ACC_R3 | ACC_DESC | ACC_DATA_WR,
+					FLAG_GRAN4K | FLAG_32BIT);
+
+	gdt_flush(); 
 }
